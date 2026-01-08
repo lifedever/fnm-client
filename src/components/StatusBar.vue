6ad3a5a0265bfcd70274c665d7e566278bb87662
Timothy.Ge <gefangshuai@outlook.com>
2026-01-08 22:55:24 +0800

    feat: 状态栏添加 GitHub 仓库链接

--- src/components/StatusBar.vue
+++ src/components/StatusBar.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { computed, ref, onMounted } from "vue";
-import { NSpace, NText, NDivider, NTag } from "naive-ui";
+import { NSpace, NText, NDivider, NTag, NButton, NTooltip } from "naive-ui";
+import { LogoGithub } from "@vicons/ionicons5";
 import { useVersionStore } from "@/stores/version";
 import { getVersion } from "@tauri-apps/api/app";
+import { openUrl } from "@tauri-apps/plugin-opener";
 
 const versionStore = useVersionStore();
 const appVersion = ref("");
@@ -20,6 +22,10 @@ const statusText = computed(() => {
 });
 
 const installedCount = computed(() => versionStore.installedVersions.length);
+
+function openGitHub() {
+  openUrl("https://github.com/lifedever/fnm-client");
+}
 </script>
 
 <template>
@@ -41,7 +47,19 @@ const installedCount = computed(() => versionStore.installedVersions.length);
         </NSpace>
       </NSpace>
 
-      <NText depth="3" style="font-size: 12px"> fnm GUI v{{ appVersion }} </NText>
+      <NSpace align="center" :size="8">
+        <NText depth="3" style="font-size: 12px"> fnm GUI v{{ appVersion }} </NText>
+        <NTooltip>
+          <template #trigger>
+            <NButton quaternary circle size="tiny" @click="openGitHub">
+              <template #icon>
+                <LogoGithub />
+              </template>
+            </NButton>
+          </template>
+          GitHub
+        </NTooltip>
+      </NSpace>
     </NSpace>
   </div>
 </template>
